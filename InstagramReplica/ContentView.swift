7ee94dbebe66ff7d6943a33d6c25c64be1ee0bfe
RozhinaNadya <zhuk.09@mail.ru>
2022-07-25 14:46:43 +0200

    Created Navigation bar

--- InstagramReplica/ContentView.swift
+++ InstagramReplica/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView(.vertical) {
+                
+            }
+            .navigationTitle("Replica")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image(systemName: "chevron.left")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "ellipsis")
+                }
+            }
+        }
     }
 }
 
