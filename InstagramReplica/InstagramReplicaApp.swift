4ff4245af70750e17ed11bdde5ad32d06290f42e
RozhinaNadya <zhuk.09@mail.ru>
2022-07-25 14:22:12 +0200

    Initial Commit

---
+++ InstagramReplica/InstagramReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramReplicaApp.swift
+//  InstagramReplica
+//
+//  Created by Надежда on 2022-07-25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
