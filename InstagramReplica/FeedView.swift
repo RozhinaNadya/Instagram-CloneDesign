8030bcb95337ed4fa99a2b70557136e7db8b664b
RozhinaNadya <zhuk.09@mail.ru>
2022-07-25 16:25:50 +0200

    Created FeedView and added stories block

---
+++ InstagramReplica/FeedView.swift
@@ -0,0 +1,63 @@
+//
+//  FeedView.swift
+//  InstagramReplica
+//
+//  Created by Надежда on 2022-07-25.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical) {
+                statusView
+            }
+            .navigationTitle("Instagram Copy")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image(systemName: "camera")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                }
+            }
+        }
+    }
+    
+    private var statusView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 12) {
+                Image("avatar")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 75, height: 75)
+                    .clipShape(Circle())
+                    .overlay(
+                        Image(systemName: "plus")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                            .background(Circle().fill(Color.blue))
+                            .padding(.leading, 8)
+                        , alignment: .bottomTrailing
+                    )
+                
+                ForEach(1..<5) { index in
+                    Image("img\(index)")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 75, height: 75)
+                        .clipShape(Circle())
+                        .overlay(Circle().strokeBorder(Color.orange, lineWidth: 2))
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
--- InstagramReplica/InstagramReplicaApp.swift
+++ InstagramReplica/InstagramReplicaApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct InstagramReplicaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+         //   ContentView()
+            FeedView()
         }
     }
 }
